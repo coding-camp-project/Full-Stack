25ff48ffbd30c71e9502074656faa68447ba0511
iqbalapriand <iqbalapriand@gmai.com>
2026-05-07 23:20:47 +0700

    landing page v1

--- frontend-nutrify/src/App.jsx
+++ frontend-nutrify/src/App.jsx
@@ -1,12 +1,7 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-import LandingPage from "/capstone/Full-Stack/frontend-nutrify/src/pages/Landingpage.jsx";
-import './App.css'
+import LandingPage from "./pages/LandingPage"
 
 function App() {
-  return <LandingPage />;
+  return <LandingPage />
 }
 
-export default App;
+export default App
--- frontend-nutrify/src/pages/LandingPage.jsx
+++ frontend-nutrify/src/pages/LandingPage.jsx
@@ -1,17 +1,19 @@
-import Navbar from "../sections/Navbar";
-import Hero from "../sections/Hero";
-import Features from "../sections/Features";
-import Footer from "../sections/Footer";
+import Navbar from "../sections/Navbar"
+import Hero from "../sections/Hero"
+import Specialization from "../sections/Specialization"
+import NutrifyFeatures from "../sections/NutrifyFeatures"
+import WhyChoose from "../sections/WhyChoose"
 
 function LandingPage() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Features />
-      <Footer />
+      <Specialization />
+      <NutrifyFeatures />
+      <WhyChoose />
     </>
-  );
+  )
 }
 
-export default LandingPage;
+export default LandingPage
