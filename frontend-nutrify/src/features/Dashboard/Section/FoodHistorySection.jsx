ce855017a7da484c24111ef1d10f02239839f6b7
Faqih Ikhtibar <faqihikhtibar@gmail.com>
2026-05-12 21:37:34 +0700

    feat: make food cards and calories line chart

---
+++ frontend-nutrify/src/features/Dashboard/Section/FoodHistorySection.jsx
@@ -0,0 +1,47 @@
+
+
+import FoodHistoryCard from "../Component/FoodHistoryCard";
+
+function FoodHistorySection() {
+  const foods = [
+    {
+      title: "Nasi Goreng",
+      time: "Hari ini, 13.00",
+      components: "4 komponen",
+      image:
+        "https://images.unsplash.com/photo-1512058564366-18510be2db19?q=80&w=1200",
+    },
+
+    {
+      title: "Nasi Goreng",
+      time: "Hari ini, 13.00",
+      components: "4 komponen",
+      image:
+        "https://images.unsplash.com/photo-1512058564366-18510be2db19?q=80&w=1200",
+    },
+
+    {
+      title: "Nasi Goreng",
+      time: "Hari ini, 13.00",
+      components: "4 komponen",
+      image:
+        "https://images.unsplash.com/photo-1512058564366-18510be2db19?q=80&w=1200",
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {foods.map((food, index) => (
+        <FoodHistoryCard
+          key={index}
+          title={food.title}
+          time={food.time}
+          components={food.components}
+          image={food.image}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default FoodHistorySection;
